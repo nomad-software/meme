9eb825e468b6832f6e0c7cf6f37e15fb9fcd7f3a
Gary Willoughby <dev@nomad.so>
2016-12-26 22:50:49 +0000

    Added comments to the output package.

--- output/output.go
+++ output/output.go
@@ -7,6 +7,7 @@ import (
 	"github.com/fatih/color"
 )
 
+// Print an error and exit the program if the error is non nil.
 func OnError(err error, text string) {
 	if err != nil {
 		fmt.Fprintln(os.Stderr, color.RedString(text+": %s", err.Error()))
@@ -14,11 +15,13 @@ func OnError(err error, text string) {
 	}
 }
 
+// Print an error and exit the program.
 func Error(text string) {
 	fmt.Fprintln(os.Stderr, color.RedString(text))
 	os.Exit(1)
 }
 
+// Print information.
 func Info(format string, args ...interface{}) {
 	fmt.Printf(color.GreenString(format)+"\n", args...)
 }
