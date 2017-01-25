9cd4a2305a480a3f110262558fe5fcf05e2b1c6d
Gary Willoughby <dev@nomad.so>
2017-01-25 21:24:26 +0000

    Updated the gif renderer to use concurrency for faster rendering.

--- image/renderer.go
+++ image/renderer.go
@@ -49,39 +49,66 @@ func RenderImage(opt cli.Options, st stream.Stream) stream.Stream {
 	return stream.EncodeImage(ctx.Image())
 }
 
+// A unit of work containing frame information.
+type frameInfo struct {
+	base   image.Rectangle
+	frame  *image.Paletted
+	index  int
+	top    string
+	bottom string
+}
+
+// Process each gif frame.
+func processFrame(fi frameInfo, output chan frameInfo) {
+	// Expand each frame, if needed, so it's the same size as the base.
+	// This is to make it easier to draw and position the text.
+	img := image.NewPaletted(fi.base, fi.frame.Palette)
+	draw.Draw(img, fi.frame.Bounds(), fi.frame, fi.frame.Bounds().Min, draw.Src)
+
+	// resImg, factor := resizeImage(img)
+	// resBounds := resizeBounds(frame.Bounds(), factor)
+	// src.Config.Width = resImg.Bounds().Dx()
+	// src.Config.Height = resImg.Bounds().Dy()
+
+	// Draw on the text.
+	ctx := gfx.NewContext(img)
+	if fi.top != "" {
+		gfx.TopBanner(ctx, fi.top)
+	}
+	if fi.bottom != "" {
+		gfx.BottomBanner(ctx, fi.bottom)
+	}
+
+	// Convert the context to a paletted image.
+	img = image.NewPaletted(img.Bounds(), img.Palette)
+	draw.FloydSteinberg.Draw(img, img.Bounds(), ctx.Image(), image.ZP)
+
+	fi.frame = img.SubImage(fi.frame.Bounds()).(*image.Paletted)
+	output <- fi
+}
+
 // RenderGif performs the graphical manipulation of the gif.
 func RenderGif(opt cli.Options, st stream.Stream) stream.Stream {
 	src := st.DecodeGif()
-	base := src.Image[0]
+	queue := make(chan frameInfo)
 
 	for x, frame := range src.Image {
-		// Expand each frame, if needed, so it's the same size as the base.
-		// This is to make it easier to draw and position the text.
-		img := image.NewPaletted(base.Bounds(), frame.Palette)
-		draw.Draw(img, frame.Bounds(), frame, frame.Bounds().Min, draw.Src)
-
-		// resImg, factor := resizeImage(img)
-		// resBounds := resizeBounds(frame.Bounds(), factor)
-		// src.Config.Width = resImg.Bounds().Dx()
-		// src.Config.Height = resImg.Bounds().Dy()
-
-		// Draw on the text.
-		ctx := gfx.NewContext(img)
-		if opt.Top != "" {
-			gfx.TopBanner(ctx, opt.Top)
+		fi := frameInfo{
+			base:   src.Image[0].Bounds(),
+			frame:  frame,
+			index:  x,
+			top:    opt.Top,
+			bottom: opt.Bottom,
 		}
-		if opt.Bottom != "" {
-			gfx.BottomBanner(ctx, opt.Bottom)
-		}
-
-		// Convert the context to a paletted image.
-		img = image.NewPaletted(img.Bounds(), frame.Palette)
-		draw.FloydSteinberg.Draw(img, img.Bounds(), ctx.Image(), image.ZP)
-
-		// Replace the frame.
-		src.Image[x] = img.SubImage(frame.Bounds()).(*image.Paletted)
+		go processFrame(fi, queue)
 	}
 
+	for range src.Image {
+		fi := <-queue
+		src.Image[fi.index] = fi.frame
+	}
+
+	close(queue)
 	return stream.EncodeGif(src)
 }
 
